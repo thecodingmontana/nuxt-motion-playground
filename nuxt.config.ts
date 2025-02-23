3f74f051ffbddf4735090429a743e2f675e9cbb3
thecodingmontana <dev.humblechris254@gmail.com>
2025-02-23 18:22:02 +0300

    chore(app): add eslint configurations and commintlint rules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  modules: ['@nuxt/eslint'],
+  eslint: {
+    config: {
+      stylistic: true
+    }
+  }
 })
