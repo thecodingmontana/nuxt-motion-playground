2cb0dd632038b3492293e320d0120fe21baa1963
thecodingmontana <dev.humblechris254@gmail.com>
2025-03-01 11:25:27 +0300

    chore(app): add animation on the images for the smooth transition

--- pages/creations/smooth-transition/index.vue
+++ pages/creations/smooth-transition/index.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { Button } from '~/components/ui/button'
-
 definePageMeta({
   layout: 'creations',
 })
@@ -25,21 +23,24 @@ onMounted(async () => {
   timeline = $gsap.timeline({ paused: true })
 
   timeline
-    .fromTo('.animate-text h2', { y: 20, opacity: 0 }, { y: 0, opacity: 1, duration: 0.5, ease: 'power2.out', delay: 0.6 })
-    .fromTo('.animate-text div p', { opacity: 0, y: 20 }, { y: 0, opacity: 1, duration: 0.3, ease: 'power1.out' })
-    .fromTo('.animate-text div button', { opacity: 0, y: 20 }, { y: 0, opacity: 1, duration: 0.3, ease: 'power1.out' })
-    .fromTo(
-      '.grid .industrial, .grid .industrial img, .grid .commercial, .grid .commercial img, .grid .residential, .grid .residential img',
-      { scaleY: 0, transformOrigin: 'bottom' },
-      { scaleY: 1, duration: 1.2, ease: 'power2.out', stagger: 0.2, delay: 0.5 },
-    )
-    .call(() => {
-      $gsap.to('.grid .industrial div, .grid .commercial div, .grid .residential div', {
-        opacity: 1,
-        duration: 0.5,
-        ease: 'power2.out',
-      })
-    })
+    .fromTo('.bottom-left', { y: 20, opacity: 0 }, { y: 0, opacity: 1, duration: 0.6, ease: 'power2.out', delay: 0.6 })
+    .fromTo('.bigger-img', { opacity: 0, y: 20 }, { y: 0, opacity: 1, duration: 0.3, ease: 'cubicBezier(0.6, 0.01, -0.5, 0.95)' })
+    .fromTo('.top-right', { opacity: 0, y: 20 }, { y: 0, opacity: 1, duration: 0.3, ease: 'cubicBezier(0.6, 0.01, -0.5, 0.95)' })
+    .fromTo('.bottom-right', { opacity: 0, y: 20 }, { y: 0, opacity: 1, duration: 0.3, ease: 'cubicBezier(0.6, 0.01, -0.5, 0.95)' })
+    .fromTo('.top-left', { opacity: 0, y: 20 }, { y: 0, opacity: 1, duration: 0.3, ease: 'cubicBezier(0.6, 0.01, -0.5, 0.95)' })
+
+  // .fromTo(
+  //   '.grid .industrial, .grid .industrial img, .grid .commercial, .grid .commercial img, .grid .residential, .grid .residential img',
+  //   { scaleY: 0, transformOrigin: 'bottom' },
+  //   { scaleY: 1, duration: 1.2, ease: 'power2.out', stagger: 0.2, delay: 0.5 },
+  // )
+  // .call(() => {
+  //   $gsap.to('.grid .industrial div, .grid .commercial div, .grid .residential div', {
+  //     opacity: 1,
+  //     duration: 0.5,
+  //     ease: 'power2.out',
+  //   })
+  // })
 
   timeline.play()
 })
@@ -69,60 +70,41 @@ onUnmounted(() => {
       button-class="text-black dark:text-white"
       @restart="onRestartAnimation"
     >
-      <div class="w-full h-full p-10 space-y-8">
-        <div class="space-y-3 animate-text">
-          <h2 class="text-3xl font-semibold">
-            Delivering quality workmanship
-          </h2>
-          <div class="flex items-center justify-between">
-            <p class="text-muted-foreground max-w-[27rem]">
-              Delivering exceptional plumbing solutions that save you time, money, and worry.
-            </p>
-            <Button
-              type="button"
-              class="rounded-full px-6 bg-purple-600 dark:text-white hover:bg-purple-500 drop-shadow-md shadow-md"
-            >
-              Get a quote
-            </Button>
-          </div>
+      <div class="w-full h-full p-10 relative">
+        <div class="h-[16rem] top-left w-[12rem] absolute top-3">
+          <NuxtImg
+            src="https://images.unsplash.com/photo-1587702068694-a909ef4aa346?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fG9mZmljZSUyMHNwYWNlfGVufDB8fDB8fHww"
+            alt="office space"
+            class="w-full h-full object-cover"
+          />
         </div>
-        <div class="grid grid-cols-1 pb-5 sm:pb-0 sm:grid-cols-3 items-center gap-4">
-          <div class="residential flex-shrink-0 overflow-clip rounded h-[15.5rem] relative">
-            <div class="absolute inset-0 bg-gradient-to-t from-indigo-900/70 to-transparent z-10" />
-            <div class="absolute inset-0 flex items-center justify-center z-20 p-5">
-              <p class="text-white font-semibold text-balance text-2xl text-center">
-                Residential <br> Plumbing
-              </p>
-            </div>
-            <NuxtImg
-              class="w-full h-full object-cover"
-              src="https://img.freepik.com/free-photo/high-angle-woman-working-as-plumber_23-2150746388.jpg?t=st=1740648246~exp=1740651846~hmac=efef4cf4cdbde5a154d0eef78a38c797a1f1d9360d6904d1ba21bcf2b971fcb9&w=740"
-            />
-          </div>
-          <div class="commercial flex-shrink-0 overflow-clip rounded h-[15.5rem] relative">
-            <div class="absolute inset-0 bg-gradient-to-t from-indigo-900/70 to-transparent z-10" />
-            <div class="absolute inset-0 flex items-center justify-center z-20 p-5">
-              <p class="text-white font-semibold text-balance text-2xl text-center">
-                Commercial <br> Plumbing
-              </p>
-            </div>
-            <NuxtImg
-              class="w-full h-full object-cover"
-              src="https://img.freepik.com/free-photo/steel-pipelines-cables-plant_1359-135.jpg?t=st=1740648892~exp=1740652492~hmac=125d9dd99bf9e2879bec6f2d852e7ee5d1424e1712fdd62258a3ad30e588c84c&w=1060"
-            />
-          </div>
-          <div class="industrial flex-shrink-0 overflow-clip rounded h-[15.5rem] relative">
-            <div class="absolute inset-0 bg-gradient-to-t from-indigo-900/70 to-transparent z-10" />
-            <div class="absolute inset-0 flex items-center justify-center z-20 p-5">
-              <p class="text-white font-semibold text-balance text-2xl text-center">
-                Industrial <br> Plumbing
-              </p>
-            </div>
-            <NuxtImg
-              class="w-full h-full object-cover"
-              src="https://img.freepik.com/free-photo/professional-calibrating-vacuum-pump_482257-96228.jpg?t=st=1740648935~exp=1740652535~hmac=50bf4d78bbbe28b7f56f9031c53bfdd107179d89120b0409cb5ac677b6872ac9&w=1380"
-            />
-          </div>
+        <div class="h-[16rem] top-right w-[12rem] absolute top-7 right-10">
+          <NuxtImg
+            src="https://images.unsplash.com/photo-1579487785973-74d2ca7abdd5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzh8fG9mZmljZSUyMHNwYWNlfGVufDB8fDB8fHww"
+            alt="office space"
+            class="w-full h-full object-cover"
+          />
+        </div>
+        <div class="h-[18rem] bigger-img w-[26rem] z-10 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <NuxtImg
+            src="https://images.unsplash.com/photo-1556761175-4b46a572b786?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="office space"
+            class="w-full h-full object-cover"
+          />
+        </div>
+        <div class="h-[8rem] bottom-left w-[17rem] absolute bottom-7">
+          <NuxtImg
+            src="https://images.unsplash.com/photo-1504297050568-910d24c426d3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzB8fG9mZmljZSUyMHNwYWNlfGVufDB8fDB8fHww"
+            alt="office space"
+            class="w-full h-full object-cover"
+          />
+        </div>
+        <div class="h-[8rem] bottom-right w-[17rem] absolute bottom-3 right-16 z-20">
+          <NuxtImg
+            src="https://images.unsplash.com/photo-1517502884422-41eaead166d4?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTJ8fG9mZmljZSUyMHNwYWNlfGVufDB8fDB8fHww"
+            alt="office space"
+            class="w-full h-full object-cover"
+          />
         </div>
       </div>
     </CreationsShowcase>
