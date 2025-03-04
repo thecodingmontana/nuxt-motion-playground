057a20f52183cbf72e9cf5de5f59ee3fde57a8e4
thecodingmontana <dev.humblechris254@gmail.com>
2025-03-04 10:40:26 +0300

    fix(app): gsap inertia plugin not found

--- plugins/gsap.client.ts
+++ plugins/gsap.client.ts
@@ -2,11 +2,10 @@ import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { ScrollToPlugin } from 'gsap/ScrollToPlugin'
 import { Draggable } from 'gsap/Draggable'
-import { InertiaPlugin } from 'gsap/InertiaPlugin'
 
 export default defineNuxtPlugin(() => {
   if (import.meta.client) {
-    gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, Draggable, InertiaPlugin)
+    gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, Draggable)
   }
 
   return {
