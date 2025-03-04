1439169c5da2ca6cab67cde1e809d9aa259f2b24
thecodingmontana <dev.humblechris254@gmail.com>
2025-03-04 10:32:46 +0300

    chore(app): remove movie portal

--- plugins/gsap.client.ts
+++ plugins/gsap.client.ts
@@ -2,10 +2,11 @@ import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { ScrollToPlugin } from 'gsap/ScrollToPlugin'
 import { Draggable } from 'gsap/Draggable'
+import { InertiaPlugin } from 'gsap/InertiaPlugin'
 
 export default defineNuxtPlugin(() => {
   if (import.meta.client) {
-    gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, Draggable)
+    gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, Draggable, InertiaPlugin)
   }
 
   return {
